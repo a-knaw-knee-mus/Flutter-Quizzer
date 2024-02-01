eeb57b1d65b5028296ad74712e21f43c5ddf47b2
Muhammad Mehdi Ali <64282992+a-knaw-knee-mus@users.noreply.github.com>
2024-02-01 13:08:50 -0500

    add import quiz button

---
+++ android/app/src/main/kotlin/app/vercel/muhammad/flutter_quizzer/MainActivity.kt
@@ -0,0 +1,6 @@
+package app.vercel.muhammad.flutter_quizzer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/screens/quiz_dialog_screen.dart
+++ lib/screens/quiz_dialog_screen.dart
@@ -1,10 +1,16 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_quizzer/main.dart';
+import 'package:flutter_quizzer/schema/question.dart';
 import 'package:flutter_quizzer/schema/quiz.dart';
 import 'package:flutter_quizzer/util/color_types.dart';
 import 'package:flutter_quizzer/util/form_types.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:provider/provider.dart';
+import 'package:uuid/uuid.dart';
 
 class QuizDialog extends StatefulWidget {
   final BuildContext context;
@@ -42,6 +48,129 @@ class _QuizDialogState extends State<QuizDialog> {
       _quizDescController.text = widget.quiz!.description;
     }
     super.initState();
+  }
+
+  AlertDialog getImportQuizHelpDialog() {
+    return AlertDialog(
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(32.0)),
+      ),
+      content: SizedBox(
+        height: 460,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "Select a txt file to import a quiz with its questions. \n\nA study set can be exported from Quizlet with 'Between term and definition' set to '--' and 'Between rows' set to 'New line'. Copy the text and save it to a txt file on your device along with the quiz name and description at the top.\n\nThe file should be in the format:",
+              textAlign: TextAlign.start,
+              style: GoogleFonts.robotoMono(),
+            ),
+            Container(
+                width: double.infinity,
+                padding: const EdgeInsets.all(16.0),
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: Text(
+                  '''<Quiz Name>--<Quiz Desc>\n<Term 1>--<Definition 1>\n<Term 2>--<Definition 2>\n<Term 3>--<Definition 3>\n...''',
+                  style: GoogleFonts.robotoMono(),
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+
+  void importQuiz() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+        type: FileType.custom,
+        allowMultiple: false,
+        allowedExtensions: ['txt']);
+    if (result == null) return;
+
+    try {
+      String filePath = result.files.single.path!;
+      File file = File(filePath);
+      String contents = await file.readAsString();
+      List<String> lines = contents.split('\n');
+
+      // handle quiz name and description seperately
+      String quizId = const Uuid().v1();
+      try {
+        List<String> parts = lines[0].split('--');
+        String quizName = parts[0], quizDesc = parts[1];
+        final quizBox = Hive.box<Quiz>('quizBox');
+        setState(() {
+          quizBox.put(
+            quizId,
+            Quiz(
+              name: quizName,
+              description: quizDesc,
+              createdAt: DateTime.now(),
+              updatedAt: DateTime.now(),
+            ),
+          );
+        });
+      } catch (e) {
+        if (context.mounted) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(
+                'Invalid Quiz name/description.',
+                style: GoogleFonts.jost(),
+              ),
+              duration: const Duration(
+                milliseconds: 1500,
+              ),
+              showCloseIcon: true,
+              backgroundColor: Colors.red,
+            ),
+          );
+        }
+        return;
+      }
+
+      lines.removeAt(0);
+      final questionBox = Hive.box<Question>('questionBox');
+      for (String line in lines) {
+        try {
+          List<String> parts = line.split('--');
+          String term = parts[0], definition = parts[1];
+          setState(() {
+            questionBox.put(
+              const Uuid().v1(),
+              Question(
+                term: term,
+                definition: definition,
+                quizId: quizId,
+                createdAt: DateTime.now(),
+                updatedAt: DateTime.now(),
+                isStarred: false,
+              ),
+            );
+          });
+        } catch (e) {
+          continue;
+        }
+      }
+    } catch (e) {
+      if (context.mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(
+              'Error reading file.',
+              style: GoogleFonts.jost(),
+            ),
+            duration: const Duration(
+              milliseconds: 1500,
+            ),
+            showCloseIcon: true,
+            backgroundColor: Colors.red,
+          ),
+        );
+      }
+    }
   }
 
   String getDialogTitle() {
@@ -139,7 +268,7 @@ class _QuizDialogState extends State<QuizDialog> {
       content: Form(
         key: _formKey,
         child: SizedBox(
-          height: 280,
+          height: 360,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
@@ -215,7 +344,43 @@ class _QuizDialogState extends State<QuizDialog> {
                     ),
                   ),
                 ],
-              )
+              ),
+              const Row(children: [
+                Expanded(child: Divider(color: Colors.black)),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 8),
+                  child: Text("OR"),
+                ),
+                Expanded(child: Divider(color: Colors.black)),
+              ]),
+              ElevatedButton(
+                onPressed: importQuiz,
+                style: ElevatedButton.styleFrom(
+                  elevation: 12.0,
+                  textStyle: const TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Import Quiz from Storage',
+                      style: GoogleFonts.jost(),
+                    ),
+                    IconButton(
+                        onPressed: () {
+                          showDialog(
+                            context: context,
+                            builder: (context) {
+                              return getImportQuizHelpDialog();
+                            },
+                          );
+                        },
+                        icon: const Icon(Icons.info_outline_rounded))
+                  ],
+                ),
+              ),
             ],
           ),
         ),
