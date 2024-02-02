9dc6f50e612737991bc3b8e8417f93cc28485429
Muhammad Mehdi Ali <64282992+a-knaw-knee-mus@users.noreply.github.com>
2024-02-02 10:08:25 -0500

    fix import quiz tooltip

--- lib/screens/quiz_dialog_screen.dart
+++ lib/screens/quiz_dialog_screen.dart
@@ -55,29 +55,27 @@ class _QuizDialogState extends State<QuizDialog> {
       shape: const RoundedRectangleBorder(
         borderRadius: BorderRadius.all(Radius.circular(32.0)),
       ),
-      content: SizedBox(
-        height: 460,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              "Select a txt file to import a quiz with its questions. \n\nA study set can be exported from Quizlet with 'Between term and definition' set to '--' and 'Between rows' set to 'New line'. Copy the text and save it to a txt file on your device along with the quiz name and description at the top.\n\nThe file should be in the format:",
-              textAlign: TextAlign.start,
-              style: GoogleFonts.robotoMono(),
-            ),
-            Container(
-                width: double.infinity,
-                padding: const EdgeInsets.all(16.0),
-                decoration: BoxDecoration(
-                  color: Colors.grey[200],
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-                child: Text(
-                  '''<Quiz Name>--<Quiz Desc>\n<Term 1>--<Definition 1>\n<Term 2>--<Definition 2>\n<Term 3>--<Definition 3>\n...''',
-                  style: GoogleFonts.robotoMono(),
-                ))
-          ],
-        ),
+      content: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            "Select a txt file to import a quiz with its questions. \n\nA study set can be exported from Quizlet with 'Between term and definition' set to '--' and 'Between rows' set to 'New line'. Copy the text and save it to a txt file on your device along with the quiz name and description at the top.\n\nThe file should be in the format:\n",
+            textAlign: TextAlign.start,
+            style: GoogleFonts.robotoMono(fontSize: 14),
+          ),
+          Container(
+              width: double.infinity,
+              padding: const EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Text(
+                '''<Quiz Name>--<Quiz Desc>\n<Term 1>--<Definition 1>\n<Term 2>--<Definition 2>\n<Term 3>--<Definition 3>\n...''',
+                style: GoogleFonts.robotoMono(fontSize: 14),
+              ))
+        ],
       ),
     );
   }
